168e8076c63a01e5aba2cabba84ff76472f844cf
SpencerDawg <gayforunova@gmail.com>
2025-02-26 13:28:57 -0600

    change project to see TxFM

--- Jupiter.cs
+++ Jupiter.cs
@@ -1,9 +1,14 @@
 using Terraria.ModLoader;
+using TxFM;
 
 namespace Jupiter
 {
 	public class Jupiter : Mod
 	{
-
-	}
+        public override void Load()
+        {
+            var t = new Test();
+			Test.TestMethod();
+        }
+    }
 }
